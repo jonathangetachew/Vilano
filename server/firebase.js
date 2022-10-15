f94ba95d7f5df878f9fbb0ec456cbcbf2cf06a19
Jonathan Tuta <jonathangetachew@gmail.com>
2022-10-15 18:24:10 -0500

    :sparkles: Added eslint and prettier support for the server app. Re-formatted files in the repo.

--- server/firebase.js
+++ server/firebase.js
@@ -1,6 +1,6 @@
 const firebase = require('firebase-admin');
 
-const serviceAccountKey = require('./serviceAccountKey');
+const serviceAccountKey = require('./serviceAccountKey.json');
 
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccountKey),
